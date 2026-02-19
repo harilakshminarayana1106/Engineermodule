df4417ba1072c26414e12a5a62c603a8b214cf67
harilakshminarayana1106 <harilakshminarayana.jerobytes@gmail.com>
2026-02-19 12:35:46 +0530

    Use environment variables for database connection

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,35 @@
+/* ======================================================
+   LOAD ENV VARIABLES
+====================================================== */
+
+require("dotenv").config();
+
+/* ======================================================
+   PG CONNECTION
+====================================================== */
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "demo",
-  password: "12345",   
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
+/* ======================================================
+   CONNECT TEST
+====================================================== */
+
 pool.connect((err) => {
+
   if (err) {
     console.log("DB Connection Error ❌", err);
   } else {
     console.log("PostgreSQL Connected ✅");
   }
+
 });
 
 module.exports = pool;
